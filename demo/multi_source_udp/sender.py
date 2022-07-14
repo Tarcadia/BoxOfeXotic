f57d26137adbf5320cb21e65887d809f4b844d1d
Tarcadia <tarcadia@qq.com>
2022-07-15 00:01:29 +0800

    new sender implementation

--- demo/multi_source_udp/sender.py
+++ demo/multi_source_udp/sender.py
@@ -13,32 +13,32 @@ queue = [];
 def server_thread():
     while True:
         recv, addr = server.recvfrom(1500);
-        info = int.from_bytes(recv, byteorder='big', signed=False);
+        info = recv.decode(config.ENCODING);
         queue.append([config.SEND_REPEAT, addr]);
-        print("START %s: %s:%s" % (info, *addr));
+        print("REQ FROM %s:%s >> %s" % (*addr, info));
+        # server.sendto(recv, addr);
+        server.sendto(config.SEND_DATA, addr);
 
 def sender_thread():
     sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
     addr = (config.SERVER_HOST, random.choice(config.SENDER_PORT_RANGE));
     try:
         sender.bind(addr);
+        print("SENDER: %s:%s" % addr);
     except OSError as err:
+        print("SENDER: NOT BOUND");
         return;
-    print("SENDER: %s:%s" % addr);
     while True:
         try:
             target = queue.pop(0);
         except IndexError as err:
-            time.sleep(0.1);
             continue;
         sender.sendto(config.SEND_DATA, target[1]);
-        print("SEND: %s:%s by %s:%s" % (*target[1], *addr));
+        print("SEND TO %s:%s FROM %s:%s" % (*target[1], *addr));
         target[0] -= 1;
         if (target[0] > 0):
+            time.sleep(0.1);
             queue.append(target);
-        else:
-            print("END: %s:%s" % target[1]);
-        time.sleep(0.1);
 
 th_server = threading.Thread(target=server_thread);
 th_server.start();
