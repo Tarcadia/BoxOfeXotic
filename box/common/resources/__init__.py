6c75fe9b94f60357ae952be14b9d0f00ff5fc3aa
Tarcadia <tarcadia@qq.com>
2023-10-24 23:27:12 +0800

    :construction: box/common/resources: Introduce EmptyProcessor to represent not found processors.

--- box/common/resources/__init__.py
+++ box/common/resources/__init__.py
@@ -1,7 +1,7 @@
 
 
 from ._living import Living
-from ._processor import Processor
+from ._processor import Processor, EmptyProcessor, EMPTY_PROCESSOR_TTL
 from ._resource import Resource, EmptyResource, EMPTY_RESOURCE_TTL
 
 from ._protocol import address_to_host_port, get_address
@@ -13,6 +13,8 @@ from ._protocol import BOX_DEFAULT_PORT
 __all__ = (
     "Living",
     "Processor",
+    "EmptyProcessor",
+    "EMPTY_PROCESSOR_TTL",
     "Resource",
     "EmptyResource",
     "EMPTY_RESOURCE_TTL",
--- box/common/resources/_processor.py
+++ box/common/resources/_processor.py
@@ -2,11 +2,14 @@
 
 from dataclasses import dataclass
 from math import nan
+from time import time
 
 from ._living import Living
 from ._protocol import address_to_host_port, get_address
 
 
+
+EMPTY_PROCESSOR_TTL = 1
 
 @dataclass
 class Processor(Living):
@@ -17,3 +20,29 @@ class Processor(Living):
 
     def __post_init__(self):
         self.address = get_address(*address_to_host_port(self.address))
+
+
+@dataclass
+class EmptyProcessor(Processor):
+    ttl             : float                 = 0
+    timestamp       : float                 = 0
+    address         : str                   = ""
+    token           : str                   = ""
+    desc            : str                   = ""
+    usage           : float                 = nan
+
+    @property
+    def ttl(self):
+        return EMPTY_PROCESSOR_TTL
+    
+    @ttl.setter
+    def ttl(self, _ttl):
+        return _ttl
+
+    @property
+    def timestamp(self):
+        return time()
+    
+    @timestamp.setter
+    def timestamp(self, _timestamp):
+        return _timestamp
